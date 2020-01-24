084013ed57d7c7b2e0e87e53e130247c6aadc218
Taylor Thurlow <taylorthurlow@me.com>
2020-01-23 21:32:59 -0800

    Refactor Installer

--- src/build/installer.cr
+++ src/build/installer.cr
@@ -9,47 +9,50 @@ module Build
       @arch = arch
     end
 
+    # Install a specific *crystal_version* to an *install_directory*.
     def install(crystal_version : String, install_directory : Path)
       url = @source.url_for(crystal_version, @platform, @arch)
-
       puts "Downloading from #{@source.name} with URL: #{url}"
-      puts "Installing to path: #{install_directory}"
 
-      # The name of the tar file we're about to download
-      filename = url.split("/").last
-
-      # The absolute path to save the tar file to
-      target_file = File.join(install_directory.to_s, filename)
-
-      # Create any directories necessary
-      Dir.mkdir_p(File.dirname(target_file))
-
-      # Check if the target file location is writable
-      if !File.writable?(File.dirname(target_file))
-        puts "Target not writable: #{File.dirname(target_file)}"
-        exit 1
-      end
-
-      # Check if we've already downloaded the file
-      # TODO: Should probably utilize some sort of checksum comparison here
-      if File.exists?(target_file)
-        puts "Target file exists, skipping download."
-      else
-        Crest.get(url) { |resp| File.write(target_file, resp.body_io) }
-      end
+      target_file_path = prepare_file_download(install_directory.to_s, url)
 
       # Extract the downloaded tar file, giving us the name of the top-level
       # directory that Crystal was extracted to
       root_directory = extract_tar(target_file)
 
       # Rename the root directory to just be the version number
-      move_from = File.expand_path(File.join(File.dirname(target_file), root_directory))
-      move_to = File.expand_path(File.join(File.dirname(target_file), crystal_version))
-
+      move_from = File.expand_path(File.join(File.dirname(target_file_path), root_directory))
+      move_to = File.expand_path(File.join(File.dirname(target_file_path), crystal_version))
       system("mv #{move_from} #{move_to}")
 
       # Remove the tar file now that we're done with it
-      FileUtils.rm(target_file)
+      FileUtils.rm(target_file_path)
+    end
+
+    # Given the *url* for the tarball to be downloaded and the directory in
+    # which to install Crystal, do some preparation for the downloading
+    # process, and return the absolute path to where the file should be
+    # downloaded.
+    private def prepare_file_download(install_directory : String, url : String) : String
+      filename = url.split("/").last
+      target_file_path = File.join(install_directory, filename)
+      Dir.mkdir_p(File.dirname(target_file_path)) # Create the directory if necessary
+
+      # Check if the target file location is writable
+      if !File.writable?(File.dirname(target_file_path))
+        puts "Target not writable: #{File.dirname(target_file_path)}"
+        exit 1
+      end
+
+      # Check if we've already downloaded the file
+      # TODO: Should probably utilize some sort of checksum comparison here
+      if File.exists?(target_file_path)
+        STDERR.puts "Target file exists, skipping download."
+      else
+        Crest.get(url) { |resp| File.write(target_file_path, resp.body_io) }
+      end
+
+      target_file_path
     end
 
     # Extract the tar file to the directory it is contained within, and return
